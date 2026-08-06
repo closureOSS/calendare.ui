e8adcb9466b65f63a0aef2703752c642fcbc23d3
Robert Bill <147130488+RobSlgm@users.noreply.github.com>
2026-08-06 20:31:09 +0200

    Dependencies updates and UI adaptions (#18)
    * Sync a9uitemplate updates
    
    * Adapt hint-box and value-list

--- projects/admin/src/app/a9uitemplate/label-value-list/index.ts
+++ projects/admin/src/app/a9uitemplate/label-value-list/index.ts
@@ -1,10 +1,14 @@
 import { LabelValueList } from "./label-value-list";
 import { LabelValueItem } from "./label-value-item";
+import { LabelValueItemLabel, LabelValueItemValue } from "./label";
 
 export * from './label-value-list';
 export * from './label-value-item';
+export * from './label';
 
 export const A9LabelValueListImports = [
   LabelValueList,
   LabelValueItem,
+  LabelValueItemLabel,
+  LabelValueItemValue,
 ] as const;
